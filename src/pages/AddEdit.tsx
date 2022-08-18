5881d0e1bc750c6fbd0187e36dc097e00850e7d2
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-08-18 15:41:59 +0530

    Added Edit component

--- src/pages/AddEdit.tsx
+++ src/pages/AddEdit.tsx
@@ -1,7 +1,77 @@
-import './AddEdit.css'
+import "./AddEdit.css";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useAddContactMutation } from "../services/contactsApi";
+
+const initialState = {
+  name: "",
+  email: "",
+  contact: "",
+};
 
 const AddEdit = () => {
-  return <h2>AddEdit</h2>;
+  const [formValue, setFormValue] = useState(initialState);
+  const { name, email, contact } = formValue;
+  const [addContact] = useAddContactMutation();
+  const navigate = useNavigate();
+  const handleInputChange = (e: any) => {
+    let { name, value } = e.target;
+    setFormValue({ ...formValue, [name]: value });
+  };
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    if (!name && !email && !contact) {
+      toast.error("Please Enter all the details before submitting");
+    } else {
+      await addContact(formValue);
+      navigate("/");
+      toast.success("Successfully added contact");
+    }
+  };
+
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          margin: "auto",
+          padding: "15px",
+          maxWidth: "400px",
+          alignContent: "center",
+        }}
+      >
+        <label htmlFor="name">Name</label>
+        <input
+          value={name}
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Enter your Name"
+          onChange={handleInputChange}
+        />
+        <label htmlFor="name">Email</label>
+        <input
+          value={email}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Enter your Email"
+          onChange={handleInputChange}
+        />
+        <label htmlFor="contact">Contact</label>
+        <input
+          value={contact}
+          type="number"
+          name="contact"
+          id="contact"
+          placeholder="Enter your Contact"
+          onChange={handleInputChange}
+        />
+        <input type="submit" value="Add" />
+      </form>
+    </div>
+  );
 };
 
 export default AddEdit;
