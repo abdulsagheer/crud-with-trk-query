817801ffdad2754aa7723fd5a78918af35ef9561
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-08-18 15:42:08 +0530

    Configured Store

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,75 @@
 import "./Home.css";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import {
+  useContactsQuery,
+  useDeleteContactMutation,
+} from "../services/contactsApi";
 
 const Home = () => {
-  return <h2>Home</h2>;
+  const { data, isLoading, error } = useContactsQuery();
+  const [deleteContact] = useDeleteContactMutation();
+  const handleDelete = async (id: any) => {
+    if (window.confirm("Are you sure you want to delete this contact?")) {
+      await deleteContact(id);
+      toast.success("Successfully deleted this contact");
+    }
+  };
+  useEffect(() => {
+    if (error) {
+      toast.error("Something went wrong");
+    }
+  }, [error]);
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <Link to="/addContact">
+        <button className="btn btn-add">Add Contact</button>
+      </Link>
+      <br />
+      <br />
+      <table className="styled-table">
+        <thead>
+          <tr>
+            <th style={{ textAlign: "center" }}>Sl.no</th>
+            <th style={{ textAlign: "center" }}>Name</th>
+            <th style={{ textAlign: "center" }}>Email</th>
+            <th style={{ textAlign: "center" }}>Contact</th>
+            <th style={{ textAlign: "center" }}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((item: any, index: any) => {
+            return (
+              <tr key={item.id}>
+                <th scope="row">{index + 1}</th>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.contact}</td>
+                <td>
+                  <Link to={`/editContact/${item.id}`}>
+                    <button className="btn btn-edit">Edit</button>
+                  </Link>
+                  <button
+                    className="btn btn-delete"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    Delete
+                  </button>
+                  <Link to={`/info/${item.id}`}>
+                    <button className="btn btn-view">View</button>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Home;
